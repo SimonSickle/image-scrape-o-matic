8c0ad2a0e352377d763aeacd963b0329d09353a8
Simon Sickle <simon@simonsickle.com>
2016-10-30 18:26:51 -0500

    Add more logic
    Signed-off-by: Simon Sickle <simon@simonsickle.com>

--- scraper.sh
+++ scraper.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
-# Copyright 2016 Simon Sickle <simon@simonsickle.com>, under AGPLv3.0
+#    This is a image scraper for all website pages
+#    Copyright (C) 2016  Simon Sickle <simon@simonsickle.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 # $1 is the site name w/ subdirectory (eg simonsickle.com/blog/post1/) to scrape
+
 COUNTER=0
 urls=$(curl $1 | grep .jpg)
 baseURL=$(echo $1 | cut -d'/' -f3)
@@ -19,4 +36,7 @@ done
 
 echo "Downloaded $COUNTER files"
 
-osascript toPhotos.applescript
+# Import script if macOS
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    osascript toPhotos.applescript
+fi
--- toPhotos.applescript
+++ toPhotos.applescript
@@ -1,4 +1,19 @@
--- Copyright 2016 Simon Sickle <simon@simonsickle.com>, under AGPLv3.0
+--    This is an apple script to import all photos you just scraped into an album
+--    Copyright (C) 2016  Simon Sickle <simon@simonsickle.com>
+--
+--    This program is free software: you can redistribute it and/or modify
+--    it under the terms of the GNU Affero General Public License as
+--    published by the Free Software Foundation, either version 3 of the
+--    License, or (at your option) any later version.
+--
+--    This program is distributed in the hope that it will be useful,
+--    but WITHOUT ANY WARRANTY; without even the implied warranty of
+--    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--    GNU Affero General Public License for more details.
+--
+--    You should have received a copy of the GNU Affero General Public License
+--    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 on run
 	set folderList to (choose folder without multiple selections allowed)
 	display dialog "What album to import to" default answer "Backgrounds"
